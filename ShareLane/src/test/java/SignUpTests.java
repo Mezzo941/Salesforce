4de64e9e6d220696748d34c4e5ff03e39ac4d2df
Vsevolod <mezzo9411@gmail.com>
2021-08-27 02:31:31 +0300

    feature/add some dirty tests

--- ShareLane/src/test/java/SignUpTests.java
+++ ShareLane/src/test/java/SignUpTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class SignUpTests {
 
@@ -58,8 +59,57 @@ public class SignUpTests {
         driver.findElement(By.name("password2")).sendKeys("1234");
         driver.findElement(By.cssSelector("[value=Register]")).click();
         String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
-        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used",
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data " +
+                        "or email was previously used",
                 "Registration with same Email that already used must not be possible");
         driver.quit();
     }
+
+    @Test
+    public void registrationWithEmptyFieldsShouldBeDecline() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("11111");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        boolean isDisplay = driver.findElement(By.cssSelector("[class=error_message]")).isDisplayed();
+        assertTrue(isDisplay);
+        driver.quit();
+    }
+
+    @Test
+    public void registrationWithInvalidEmailData() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("11111");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Seva");
+        driver.findElement(By.name("email")).sendKeys("asking1234mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("1234");
+        driver.findElement(By.name("password2")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        boolean isDisplay = driver.findElement(By.cssSelector("[class=error_message]")).isDisplayed();
+        assertTrue(isDisplay);
+        driver.quit();
+    }
+
+    @Test
+    public void registrationWithNotExistsEmail() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("11111");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Seva");
+        driver.findElement(By.name("email")).sendKeys("a@b.b");
+        driver.findElement(By.name("password1")).sendKeys("1234");
+        driver.findElement(By.name("password2")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        boolean isDisplay = driver.findElement(By.cssSelector("[class=error_message]")).isDisplayed();
+        assertTrue(isDisplay);
+        driver.quit();
+    }
+
 }
