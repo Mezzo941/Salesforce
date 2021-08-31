dc4fec76b72d7bf085d9fa008967c14e397f2985
Vsevolod <mezzo9411@gmail.com>
2021-08-31 20:19:12 +0300

    feature: add HoversTest

--- herokuapp/src/test/java/TypoTest.java
+++ herokuapp/src/test/java/TypoTest.java
@@ -7,7 +7,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class TypoTest {
