8d9a7a11349aebba82b793fdfa072b7d7d7e543a
Vsevolod <mezzo9411@gmail.com>
2021-09-09 15:28:43 +0300

    feature: add negative validation tests

---
+++ saucedemo/src/test/java/NegativeValidationTests.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NegativeValidationTests extends BaseTest {
+
+    @Test
+    public void LoginWithEmptyUsernameIsImpossible() {
+        driver.get("https://www.saucedemo.com");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("[data-test=error]")).getText(), "Epic sadface: Password is required");
+    }
+
+
+    @Test
+    public void LoginWithEmptyFieldsIsImpossible() {
+        driver.get("https://www.saucedemo.com");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("[data-test=error]")).getText(), "Epic sadface: Username is required");
+    }
+
+    @Test
+    public void LoginWithNotValidDateIsImpossible(){
+        driver.get("https://www.saucedemo.com");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("standard_user");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("[data-test=error]")).getText(), "Epic sadface: Username and password do not match any user in this service");
+    }
+
+}
