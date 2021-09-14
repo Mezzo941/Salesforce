904860af2785088cb18c8f461d8781696a9fda8e
Vsevolod <mezzo9411@gmail.com>
2021-09-14 16:18:00 +0300

    feature: merge 2 classes in 1

--- saucedemo/src/test/java/tests/BaseTest.java
+++ saucedemo/src/test/java/tests/BaseTest.java
@@ -1,34 +1,33 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.CatalogPage;
 import pages.LoginPage;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-    static final By NO_VALID_DATE_ERRORS = By.cssSelector("[data-test=error]");
-    static final By BLOCKED_USER_ERROR = By.cssSelector("[data-test=error]");
-
     protected WebDriver driver;
     protected LoginPage loginPage;
+    protected CatalogPage catalogPage;
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(new ChromeOptions().addArguments("headless"));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
+        catalogPage = new CatalogPage(driver);
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
