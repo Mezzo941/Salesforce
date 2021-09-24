1a02778e6506da6369d6088ea1e290571f13ef90
Vsevolod <mezzo9411@gmail.com>
2021-09-24 19:56:18 +0300

    feature: optimize negative login test

--- saucedemo/src/test/java/tests/LoginPageTest.java
+++ saucedemo/src/test/java/tests/LoginPageTest.java
@@ -1,45 +1,41 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class LoginPageTest extends BaseTest{
+public class LoginPageTest extends BaseTest {
+
+    @DataProvider(name = "noValidData")
+    public Object[][] data() {
+        return new Object[][]{
+                {"standard_user", "", "Epic sadface: Password is required"},
+                {"", "", "Epic sadface: Username is required"},
+                {"standard_user","standard_user","Epic sadface: Username and password do not match any user in this service"},
+        };
+    }
 
     @Test
-    public void ValidDataUserSuccessfulLogin(){
+    public void ValidDataUserSuccessfulLogin() {
         loginPage.open();
-        loginPage.authorization("standard_user","secret_sauce");
+        loginPage.authorization("standard_user", "secret_sauce");
         Assert.assertTrue(catalogPage.isOpened());
     }
 
     @Test
-    public void lockedUserCantLogin(){
+    public void lockedUserCantLogin() {
         loginPage.open();
-        loginPage.authorization("locked_out_user","secret_sauce");
+        loginPage.authorization("locked_out_user", "secret_sauce");
         Assert.assertEquals(loginPage.getDynamicError(), "Epic sadface: Sorry, this user has been locked out.");
     }
 
 
-    @Test
-    public void LoginWithEmptyPasswordIsImpossible() {
+    @Test(dataProvider = "noValidData")
+    public void noValidDataInputBreakSuccess(String userName,String lastName, String error) {
         loginPage.open();
-        loginPage.authorization("standard_user","");
-        Assert.assertEquals(loginPage.getDynamicError(), "Epic sadface: Password is required");
+        loginPage.authorization(userName,lastName);
+        Assert.assertEquals(loginPage.getDynamicError(), error);
     }
 
-
-    @Test
-    public void LoginWithEmptyFieldsIsImpossible() {
-        loginPage.open();
-        loginPage.authorization("","");
-        Assert.assertEquals(loginPage.getDynamicError(), "Epic sadface: Username is required");
-    }
-
-    @Test
-    public void LoginWithNotValidDateIsImpossible(){
-        loginPage.open();
-        loginPage.authorization("standard_user","standard_user");
-        Assert.assertEquals(loginPage.getDynamicError(), "Epic sadface: Username and password do not match any user in this service");
-    }
 
 }
